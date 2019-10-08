3ff28833329de3a7a9d4f0974ba95663708dbfa5
mms-gianni <gianni.carafa@srf.ch>
2019-10-08 16:22:24 +0200

    remove mdbvue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,22 +1,11 @@
 <template>
-    <mdb-navbar color="grey" class=" lighten-4 py-4">
-      <mdb-navbar-nav nav left>
-        <mdb-nav-item href="https://github.com">Soucre code</mdb-nav-item>
-        <mdb-nav-item href="#">Link</mdb-nav-item>
-        <mdb-nav-item href="#">Link</mdb-nav-item>
-        <mdb-nav-item href="#" disabled>Disabled</mdb-nav-item>
-      </mdb-navbar-nav>
-    </mdb-navbar>
+
 </template>
 
 <script>
-import { mdbNavbar, mdbNavbarNav, mdbNavItem } from 'mdbvue';
 export default {
   name: 'Navigation',
   components: {
-    mdbNavbar,
-    mdbNavbarNav,
-    mdbNavItem
   },
 
   props: {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
+
 import Vue from 'vue'
-import 'bootstrap-css-only/css/bootstrap.min.css'
-import 'mdbvue/build/css/mdb.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
